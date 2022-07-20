d91c2de12a2660615949bf5189ab001f45a6a5ee
RoVoth <droxer@hotmail.es>
2022-07-20 17:58:16 +0200

    interaction 1-2 and footer

--- app.js
+++ app.js
@@ -19,7 +19,15 @@ app.use(express.static(path.join(__dirname, 'public')));
 // Add the route handlers here:
 
 app.get('/', (req, res) => {
-  res.render('index');
+  res.render('index.hbs');
+});
+app.get('/beers', (req, res) => {
+  punkAPI
+    .getBeers()
+    .then(beersFromApi => {
+      res.render('beers.hbs', { beersFromApi });
+    })
+    .catch(error => console.log(error));
 });
 
 app.listen(3000, () => console.log('🏃‍ on port 3000'));
